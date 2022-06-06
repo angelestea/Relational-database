af02abdc04ead8fa85c459954a16b12be22b1883
Angel Vega <angelhidrobo2015@gmail.com>
2022-06-05 20:56:16 -0500

    first commit

---
+++ dates.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS `myTable`;
+
+CREATE TABLE `myTable` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `birth_date` varchar(255),
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Quinn Gilliam","2021-08-29"),
+  ("Cedric May","2022-01-15"),
+  ("Mary Lowe","2022-07-09"),
+  ("Mona Kelley","2022-03-06"),
+  ("Davis Tillman","2023-03-03"),
+  ("Calvin William","2023-01-15"),
+  ("Britanni Nielsen","2022-03-22"),
+  ("Chelsea Ware","2022-10-17"),
+  ("Vernon Rollins","2022-09-17"),
+  ("Kirby Suarez","2021-06-14");
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Jason Herman","2023-03-11"),
+  ("Lewis Farley","2022-01-07"),
+  ("Lysandra Pena","2021-12-04"),
+  ("Tara Mejia","2022-09-07"),
+  ("Wylie Leblanc","2022-10-06"),
+  ("Shelby Wilson","2023-02-19"),
+  ("Nigel Battle","2022-12-17"),
+  ("Jemima Alvarado","2021-12-28"),
+  ("Ivana Calderon","2023-02-23"),
+  ("Carol Robertson","2021-07-05");
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Christopher Burnett","2022-07-11"),
+  ("Phelan Baxter","2022-03-05"),
+  ("Colin Erickson","2021-12-07"),
+  ("Colorado Booth","2022-10-18"),
+  ("Ainsley Dickson","2022-04-22"),
+  ("Ariel Hartman","2022-04-15"),
+  ("Jordan Booker","2022-05-12"),
+  ("Kaye Walter","2021-04-27"),
+  ("Jacob Fletcher","2022-03-22"),
+  ("Thomas Mullins","2023-01-02");
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Rylee Blair","2021-04-28"),
+  ("Marcia Hale","2021-08-30"),
+  ("Donovan Bradford","2021-12-23"),
+  ("Emerson England","2021-08-16"),
+  ("Ignacia Small","2022-11-25"),
+  ("Yael Witt","2022-04-06"),
+  ("Calista Mcdonald","2022-06-25"),
+  ("Abra Waller","2021-07-16"),
+  ("Lucy Cash","2023-03-13"),
+  ("Ivy Kemp","2022-08-02");
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Unity Koch","2023-04-13"),
+  ("Paki Bright","2021-09-28"),
+  ("Lesley Padilla","2021-11-19"),
+  ("Jasper Dunn","2022-10-09"),
+  ("Helen Hickman","2022-10-24"),
+  ("Axel Rodgers","2022-02-24"),
+  ("Abel Wallace","2021-11-12"),
+  ("Leroy White","2021-05-29"),
+  ("Dalton Herring","2022-05-12"),
+  ("Vivien Bass","2022-10-19");
+INSERT INTO `myTable` (`name`,`birth_date`)
+VALUES
+  ("Kyla Barton","2021-09-29"),
+  ("Georgia Clements","2022-02-05"),
+  ("Kalia Curry","2021-05-25"),
+  ("Sean Koch","2023-02-28"),
+  ("Neil Delgado","2022-03-11");
---
+++ emails.sql
@@ -0,0 +1,129 @@
+DROP TABLE IF EXISTS `myTable`;
+
+CREATE TABLE `myTable` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Wilma Torres","morbi.quis.urna@aol.ca"),
+  ("Ira Mcgee","vel.nisl@protonmail.net"),
+  ("Sean Erickson","cursus.nunc@protonmail.org"),
+  ("Kylynn Ramirez","vivamus.nibh@google.edu"),
+  ("Merrill Dejesus","magna.nam.ligula@hotmail.net"),
+  ("Seth Patel","mauris@protonmail.com"),
+  ("Hall Flowers","semper@hotmail.ca"),
+  ("Astra French","consectetuer.adipiscing@hotmail.com"),
+  ("Kibo Ray","tincidunt@icloud.ca"),
+  ("Germane Washington","amet@yahoo.ca");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Dolan Hyde","pede@protonmail.com"),
+  ("Kane Lawrence","pharetra.nam@yahoo.com"),
+  ("Cyrus Perry","eu@outlook.edu"),
+  ("Ivory Barrera","cubilia@icloud.com"),
+  ("Chanda Thomas","sem.vitae.aliquam@yahoo.couk"),
+  ("Chloe Armstrong","ligula.nullam@google.org"),
+  ("Kameko Hamilton","auctor.quis.tristique@icloud.couk"),
+  ("Cairo Sanders","erat@hotmail.org"),
+  ("Camden Roberts","quisque.purus@hotmail.com"),
+  ("Lacey Bass","vel.arcu@icloud.org");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Jena Petersen","ornare.lectus.justo@outlook.org"),
+  ("Graiden Underwood","sed.nec.metus@aol.edu"),
+  ("Bruno Mullen","arcu.vestibulum@google.org"),
+  ("Jakeem Brewer","purus.accumsan@icloud.edu"),
+  ("Xyla Cole","nunc.sollicitudin@aol.com"),
+  ("Macon Cantu","sit.amet@protonmail.com"),
+  ("Laura Bartlett","nulla.eget.metus@aol.ca"),
+  ("Whoopi Moon","libero.nec@icloud.org"),
+  ("Wesley Mcmahon","ultrices.duis@protonmail.edu"),
+  ("Colton Hall","tempus.eu.ligula@outlook.com");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Veda Beck","adipiscing@aol.com"),
+  ("Meredith Sharp","ultrices.a@yahoo.edu"),
+  ("Merritt Burt","in.tempus@outlook.couk"),
+  ("Scarlet Frederick","diam@aol.net"),
+  ("Kylie Wynn","orci.adipiscing@yahoo.com"),
+  ("Buffy Watson","massa.integer.vitae@aol.net"),
+  ("Germane Bender","nibh.lacinia.orci@aol.couk"),
+  ("Marshall Osborn","nec.tempus.scelerisque@protonmail.net"),
+  ("Stephanie Buchanan","vitae.sodales.at@google.com"),
+  ("Dillon Abbott","feugiat.lorem@hotmail.com");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Gary Ayala","ullamcorper.viverra.maecenas@yahoo.net"),
+  ("McKenzie Mcpherson","sed@icloud.org"),
+  ("Abra Sims","cras@icloud.org"),
+  ("Camden Wiggins","vel.mauris@protonmail.net"),
+  ("Jerome Martinez","orci.sem@icloud.edu"),
+  ("Hyacinth Gross","nunc@protonmail.edu"),
+  ("Bernard Douglas","commodo.tincidunt.nibh@hotmail.com"),
+  ("Carl Schneider","orci.quis@outlook.org"),
+  ("Madonna Cabrera","cubilia.curae.phasellus@hotmail.edu"),
+  ("April Barrera","pellentesque@google.couk");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Megan Lane","etiam.gravida@yahoo.com"),
+  ("Nicholas Warren","dui.lectus.rutrum@aol.net"),
+  ("Colorado Herman","cum.sociis.natoque@outlook.com"),
+  ("Aladdin Estes","integer@aol.net"),
+  ("Brooke Witt","parturient.montes@google.edu"),
+  ("Seth Hess","ipsum.non@aol.org"),
+  ("Scott Sykes","nec.leo@aol.net"),
+  ("Honorato Dickerson","dolor.sit@google.net"),
+  ("Daphne Roberson","natoque.penatibus@outlook.com"),
+  ("Willow Clements","arcu.iaculis@google.com");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Merritt Anderson","elit.pretium@protonmail.org"),
+  ("Jelani Klein","porttitor.tellus@hotmail.com"),
+  ("Allegra Schultz","malesuada.integer@google.org"),
+  ("Bryar Merritt","in.hendrerit@yahoo.com"),
+  ("Hashim Montoya","morbi.tristique@hotmail.com"),
+  ("Demetrius Yates","ac@protonmail.ca"),
+  ("Rinah Patton","integer.vitae.nibh@google.org"),
+  ("Alexander Medina","massa.rutrum@aol.edu"),
+  ("Kenyon Pennington","magna.tellus.faucibus@hotmail.org"),
+  ("Cruz Conley","neque.morbi.quis@google.ca");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Hakeem Luna","pharetra.felis@icloud.org"),
+  ("Avye Bryan","mollis.vitae@hotmail.couk"),
+  ("Hanna O'donnell","aliquam.erat.volutpat@outlook.net"),
+  ("Valentine Baldwin","et.netus@yahoo.org"),
+  ("Miranda Pittman","nec.mauris@outlook.org"),
+  ("Amery Pena","pellentesque.tellus@protonmail.com"),
+  ("Tarik Wooten","odio.etiam@google.net"),
+  ("Sydney Gonzales","luctus.lobortis@icloud.org"),
+  ("Bernard Lyons","velit@google.ca"),
+  ("Gretchen Johns","ultrices.vivamus.rhoncus@protonmail.com");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Athena Maxwell","interdum.sed.auctor@google.edu"),
+  ("Cameron Bradford","amet.lorem.semper@icloud.org"),
+  ("Russell Doyle","arcu@yahoo.edu"),
+  ("Quamar Browning","mauris.rhoncus.id@icloud.edu"),
+  ("Cade Washington","ut.pellentesque@icloud.ca"),
+  ("Lucian Campos","orci@google.ca"),
+  ("Jakeem Cobb","nunc.commodo@hotmail.org"),
+  ("Xavier Travis","fringilla@aol.com"),
+  ("Karina Greene","lobortis.augue@protonmail.net"),
+  ("Price Wiley","odio@google.net");
+INSERT INTO `myTable` (`name`,`email`)
+VALUES
+  ("Kermit Lynch","tempor.arcu.vestibulum@yahoo.org"),
+  ("Ethan Kerr","donec@yahoo.com"),
+  ("Medge Stephens","eros.turpis@outlook.org"),
+  ("Karleigh Newman","elit.aliquam@hotmail.org"),
+  ("Jennifer Holden","lectus.convallis.est@outlook.edu"),
+  ("Nero Morgan","egestas.hendrerit.neque@aol.com"),
+  ("Gareth Kelly","gravida.praesent@icloud.ca"),
+  ("Bruno Terrell","proin.vel@hotmail.edu"),
+  ("Shelby Pratt","bibendum.sed@google.com"),
+  ("Lee Burgess","sed.eu@icloud.ca");
